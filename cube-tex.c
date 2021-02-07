2418cfe7e7af581698b85f51e849d20c876b17e3
Fang Tan <tanfang@uniontech.com>
2021-02-07 14:19:52 +0800

    Multiple GL_ TRIANGLE_ STRIP draw merge.

--- cube-tex.c
+++ cube-tex.c
@@ -564,12 +564,7 @@ static void draw_cube_tex(unsigned i)
 	if (gl.mode == NV12_2IMG)
 		glUniform1i(gl.textureuv, 1);
 
-	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-	glDrawArrays(GL_TRIANGLE_STRIP, 4, 4);
-	glDrawArrays(GL_TRIANGLE_STRIP, 8, 4);
-	glDrawArrays(GL_TRIANGLE_STRIP, 12, 4);
-	glDrawArrays(GL_TRIANGLE_STRIP, 16, 4);
-	glDrawArrays(GL_TRIANGLE_STRIP, 20, 4);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 24);
 }
 
 const struct egl * init_cube_tex(const struct gbm *gbm, enum mode mode, int samples)
